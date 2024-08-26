9c432fb11005936b13665872b001cea6360d1ea6
Ashutosh <ashutoshroy453@gmail.com>
2024-08-25 17:58:11 -0700

    product of two numbers

---
+++ productofnumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class productofnumbers {
+   
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+
+        System.out.println("enter Second number");
+        int b = sc.nextInt();
+
+        int product = (a*b);
+        System.out.println(product);
+
+        
+    }
+}
