867d1d321df7c6ff008646615c84b9549d5735bf
Ashutosh <ashutoshroy453@gmail.com>
2024-08-25 18:13:19 -0700

    Area of circle

---
+++ AreaofCircle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class AreaofCircle {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double rad = sc.nextDouble();
+    
+        double radius = 3.14*rad*rad;
+    
+        System.out.println(radius);
+    }
+    
+
+
+}
