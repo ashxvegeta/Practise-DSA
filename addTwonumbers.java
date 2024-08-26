44c551689cfbef6ba4559ca669c9c89c7be65698
Ashutosh <ashutoshroy453@gmail.com>
2024-08-25 17:50:12 -0700

    add two numbers

---
+++ addTwonumbers.java
@@ -0,0 +1,9 @@
+public class addTwonumbers {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        
+        System.out.println("sum:" + (a+b));
+    }
+}
