f13c4bf0b1b206fb2de6f6fb71bf2f5dd6da3e9a
Ashutosh <ashutoshroy453@gmail.com>
2024-08-26 21:52:26 -0700

    area of square

---
+++ areaOfsquare.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class areaOfsquare {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side");
+        int side = sc.nextInt();
+        int area  = side*side;
+        System.out.println("the area of square is "+ area);
+
+
+    }
+}
