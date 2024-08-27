bd357373bfd9b101f426796e30627b7522b7a6d6
Ashutosh <ashutoshroy453@gmail.com>
2024-08-26 22:36:52 -0700

    largest of two number

---
+++ largetsOftwo.java
@@ -0,0 +1,15 @@
+public class largetsOftwo {
+    
+ public static void main(String[] args) {
+ 
+    int a = 10;
+    int b = 5;
+
+    if(a>b){
+        System.out.println(a + " is greatest");
+    }else{
+        System.out.println(b +  "is greatest");
+    }
+ }
+
+}
