45765c3f4307f70dc73e0cb5d0a455d3d664a7e5
Ashutosh <ashutoshroy453@gmail.com>
2024-08-28 22:32:59 -0700

    pass Or fail

---
+++ passOrfail.java
@@ -0,0 +1,13 @@
+public class passOrfail {
+    
+
+    public static void main(String[] args) {
+
+        int marks = 40;
+
+        String status = marks>=33?"pass":"fail";
+        System.out.println(status);
+
+    }
+
+}
