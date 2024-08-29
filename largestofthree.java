39cb3f51c0f04ebf9980b700b48e986ea164eb0f
Ashutosh <ashutoshroy453@gmail.com>
2024-08-28 22:14:30 -0700

    largest of three numbers

---
+++ largestofthree.java
@@ -0,0 +1,19 @@
+public class largestofthree {
+
+    public static void main(String[] args) {
+
+        int a  =1;
+        int b = 2;
+        int c = 3;
+
+        if (a>b && a>c) {
+            System.out.println(a + "is greatest");
+        }else if(b>c){
+            System.out.println(b+ "is greatest");
+        }else{
+            System.out.println(c + "is greatest");
+        }
+
+    }
+    
+}
