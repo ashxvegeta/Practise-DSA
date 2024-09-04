4b177208080f8c4ed9690f130a33e40c702f5614
Ashutosh <ashutoshroy453@gmail.com>
2024-09-03 22:45:23 -0700

    print Sumo Of odd and Even

--- dd.java
+++ dd.java
@@ -3,15 +3,14 @@ public class dd {
     
     public static void main(String[] args) {
    
-        int number = 99880;
-        while (number>0) {
-            int lastdigit = number%10;
-            System.out.print(lastdigit);
-            number = number/10;
+      
+        int counter =1;
+        do {
 
-        }
-
-        System.out.println();
+            System.out.println("heelo");
+            counter++;
+            
+        } while (counter <= 10);
      
 
     }
