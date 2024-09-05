2793c2dd6c1b9d16e44398c21682df28354a20b9
Ashutosh <ashutoshroy453@gmail.com>
2024-09-04 22:22:06 -0700

    printTable

---
+++ printTable.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class printTable {
+ 
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int table;
+        for (int i = 1; i <= 10; i++) {
+            table = n*i;
+
+            System.out.println(table);
+        }
+
+    }
+}
