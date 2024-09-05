1e02c52be1b11c375a3a1672a03025e9f5e822f9
Ashutosh <ashutoshroy453@gmail.com>
2024-09-04 21:51:47 -0700

    factorial

---
+++ factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class factorial {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+        int fact = 1;
+
+        for (int i = n; i > 0; i--) {
+            
+            fact = fact*i;
+        }
+
+        System.out.println(fact);
+
+    }
+}
