14616da9b488919cbd8a2ee7e82c8c8e809b9dd0
Ashutosh <ashutoshroy453@gmail.com>
2024-10-02 11:11:50 -0700

    maxSubarraysum

---
+++ maxSubarraysum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class maxSubarraysum {
+    
+    public static void printMaxSubarrysum(int numbers[]) {
+        int currentsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currentsum=0;
+                for(int k = start; k<=end;k++){
+                    currentsum = currentsum + numbers[k];   
+                }
+                System.out.println(currentsum);
+                if(maxsum < currentsum){
+                    maxsum = currentsum;
+                }
+               
+            }
+            System.out.println();
+        }
+        System.out.print("the maxmimun subarray sum is "+maxsum);
+    }
+    public static void main(String[] args){
+        int numbers[] = {2,4,6,8,10};
+        printMaxSubarrysum(numbers);
+    }
+}
