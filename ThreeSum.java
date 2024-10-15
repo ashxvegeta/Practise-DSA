4368f7b7f8ff47eff0ac87d67ab8f8da476fd9ef
Ashutosh <ashutoshroy453@gmail.com>
2024-10-14 23:09:35 -0700

    ThreeSum

---
+++ ThreeSum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ThreeSum {
+
+    public List<List<Integer>> threesum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> triplet = new ArrayList<>();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[j]);
+                        triplet.add(nums[k]);
+                        Collections.sort(triplet);
+                        if (!result.contains(triplet)) {
+                            result.add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { -1, 0, 1, 2, -1, -4 };
+        ThreeSum obj = new ThreeSum();
+        System.out.println(obj.threesum(nums));
+    }
+}
