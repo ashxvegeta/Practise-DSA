86a36928be2f9a312dcc4effc0a8c639ab800a5c
Ashutosh <ashutoshroy453@gmail.com>
2024-12-05 10:21:38 -0800

    practiseSettwoDarrayquesTwo

--- practiseSettwoDarrayquesTwo.java
+++ practiseSettwoDarrayquesTwo.java
@@ -1,12 +1,18 @@
 public class practiseSettwoDarrayquesTwo {
     public static void  sumOfrowOne(int matrix[][]){
         int count =0;
-        for (int i = 0; i < matrix.length; i++) {
-             for (int j = 0; j < matrix[i].length; j++) {
-                  if(i==1){
-                    count += matrix[i][j];
-                  }
-             }
+        // for (int i = 0; i < matrix.length; i++) {
+        //      for (int j = 0; j < matrix[i].length; j++) {
+        //           if(i==1){
+        //             count += matrix[i][j];
+        //           }
+        //      }
+        // }
+
+        //only itearet 2nd row  elements
+        for (int j = 0; j < matrix[0].length; j++) {
+            count += matrix[1][j];
+          
         }
 
         System.out.println(count);
