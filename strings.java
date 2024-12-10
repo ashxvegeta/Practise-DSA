337f18577d3236a57bdb0552c26277332e191054
Ashutosh <ashutoshroy453@gmail.com>
2024-12-10 10:47:17 -0800

    strings

---
+++ strings.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class strings {
+    
+    public static  void main(String args[]){
+         
+        char arr[] = {'a', 'b', 'c', 'd', 'e'};
+        String str = "abcd";
+        String str2 =  new String("xyz");
+
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        // name =  sc.next();
+        name =  sc.nextLine();
+        System.out.println(name);
+    } 
+}
