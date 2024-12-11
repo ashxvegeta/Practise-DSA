f8ddf1e3f5ee8f9b4220506a21d295db76b33007
Ashutosh <ashutoshroy453@gmail.com>
2024-12-11 10:12:35 -0800

    strings

--- strings.java
+++ strings.java
@@ -4,15 +4,27 @@ public class strings {
     
     public static  void main(String args[]){
          
-        char arr[] = {'a', 'b', 'c', 'd', 'e'};
-        String str = "abcd";
-        String str2 =  new String("xyz");
+        // char arr[] = {'a', 'b', 'c', 'd', 'e'};
+        // String str = "abcd";
+        // String str2 =  new String("xyz");
 
 
-        Scanner sc = new Scanner(System.in);
-        String name;
-        // name =  sc.next();
-        name =  sc.nextLine();
-        System.out.println(name);
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // // name =  sc.next();
+        // name =  sc.nextLine();
+        // System.out.println(name);
+
+        //   string length
+
+        // String fullname = "tony stark";
+        // System.out.println("the length is = "+fullname.length());
+
+        //concatination
+
+        String firstname = "Ashutosh";
+        String lastname = "Roy";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
     } 
 }
