56d4c57fe880f444a4e122b1e5700be27680d365
Ashutosh <ashutoshroy453@gmail.com>
2024-12-20 10:49:08 -0800

    stringGetlargestString.java

---
+++ stringGetlargestString.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class stringGetlargestString{
+
+    public static void main(String[] args){
+
+        String fruit[] = {"apple","mango","banana"};
+        String largest = fruit[0];
+
+        for(int i=1;i<fruit.length;i++){
+           if(largest.compareTo(fruit[i])<0){
+              largest = fruit[i];
+           }
+        }
+        System.out.print(largest);
+
+    }
+
+}
