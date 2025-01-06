e33b26cf7815d813bf1129f3deffbfcab4ee18e7
Ashutosh <ashutoshroy453@gmail.com>
2025-01-06 01:58:56 -0800

    stringequals

---
+++ stringequals.java
@@ -0,0 +1,10 @@
+public class stringequals {
+    
+    public static void main(String[] args) {
+        String str= "hello";
+        String str2= "world";
+        String str3= "hello";
+
+        System.out.println(str.equals(str2)+" "+ str.equals(str3));
+    }
+}
