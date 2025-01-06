38708fd8fe5d7736f9e407b5037f1a920ab4916e
Ashutosh <ashutoshroy453@gmail.com>
2025-01-06 07:43:45 -0800

    stringreplace

---
+++ stringreplace.java
@@ -0,0 +1,8 @@
+public class stringreplace{
+
+    public static void main(String[] args){
+         String str = "helloworld".replace("l","");
+         System.out.println(str);
+         
+    }
+}
