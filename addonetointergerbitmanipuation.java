bc26cedc56210cdb6dc17906b6e200d662d55e04
Ashutosh <ashutoshroy453@gmail.com>
2025-02-17 01:45:45 -0800

    addonetointergerbitmanipuation

---
+++ addonetointergerbitmanipuation.java
@@ -0,0 +1,12 @@
+public class addonetointergerbitmanipuation {
+    
+
+    public static int addOne(int n) {
+        return -(~n);
+    }
+
+    public static void main(String[] args) {
+      System.out.println(addOne(5));       
+    }
+
+}
