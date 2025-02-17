c74693aae806d18f650842350a0795c69c989d2e
Ashutosh <ashutoshroy453@gmail.com>
2025-02-17 03:30:26 -0800

    lowerToupperUsingbits

---
+++ lowerToupperUsingbits.java
@@ -0,0 +1,11 @@
+public class lowerToupperUsingbits {
+    
+
+    public static char tolower(char c) {
+      return (char) (c | ' ');   
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tolower('M'));
+    }
+}
