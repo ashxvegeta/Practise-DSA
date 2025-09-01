b965552eaf495d6d0a4d5808db217da3f56349a1
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-01 21:52:30 +0530

    binaryStringusingRecusion.java

---
+++ binaryStringusingRecusion.java
@@ -0,0 +1,20 @@
+public class binaryStringusingRecusion {
+
+    public static void printBinaryStrings(int n , int lastplace, String str){
+
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        //kaaam
+        printBinaryStrings(n-1,0,str+"0");
+        if(lastplace==0){
+          printBinaryStrings(n-1,1,str+"1");
+        }
+    }
+    public static void main(String[] args) {
+        int n = 3; // Length of binary strings
+        int lastplace =0;
+        printBinaryStrings(n, lastplace," ");
+    }
+}
